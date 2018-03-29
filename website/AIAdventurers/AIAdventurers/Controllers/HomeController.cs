7351b84e4e1f43d2182f1d34a0e4fee2f0b43c06
nhattran <tranquannhat@gmail.com>
2018-03-28 22:27:29 -0700

    Add nutrition

--- website/AIAdventurers/AIAdventurers/Controllers/HomeController.cs
+++ website/AIAdventurers/AIAdventurers/Controllers/HomeController.cs
@@ -40,17 +40,17 @@ namespace AIAdventurers.Controllers
 
         public ActionResult FileUpload(HttpPostedFileBase file)
         {
-            string imageFilename= "";
+            string imageFilename = "";
             string filePath = "";
             if (file != null)
             {
-                 imageFilename = System.IO.Path.GetFileName(file.FileName);
+                imageFilename = System.IO.Path.GetFileName(file.FileName);
                 string imageDirectory = Server.MapPath("~/images/uploads");
                 if (!Directory.Exists(imageDirectory))
                 {
                     Directory.CreateDirectory(imageDirectory);
                 }
-                 filePath = System.IO.Path.Combine(   imageDirectory , imageFilename);
+                filePath = System.IO.Path.Combine(imageDirectory, imageFilename);
                 // file is uploaded
                 file.SaveAs(filePath);
 
@@ -66,12 +66,12 @@ namespace AIAdventurers.Controllers
             }
             string credentialFile = Server.MapPath("~") + "AIAdventurers-95702aa321b9.json";
             var credential = GoogleCredential.FromFile(credentialFile).CreateScoped(ImageAnnotatorClient.DefaultScopes);
-            var channel = new Grpc.Core.Channel( ImageAnnotatorClient.DefaultEndpoint.ToString(),  credential.ToChannelCredentials());
+            var channel = new Grpc.Core.Channel(ImageAnnotatorClient.DefaultEndpoint.ToString(), credential.ToChannelCredentials());
 
             var image = Google.Cloud.Vision.V1.Image.FromFile(filePath);
-       
+
             var client = ImageAnnotatorClient.Create(channel);
-            
+
             var response = client.DetectLabels(image);
             foreach (var annotation in response)
             {
@@ -81,9 +81,20 @@ namespace AIAdventurers.Controllers
 
             VisionViewModel model = new VisionViewModel();
             model.imageUrl = "/images/uploads/" + imageFilename;
-            model.GoogleVisionResults = response.ToString();
+            List<Ingredient> ingredients = new List<Ingredient>();
+            ingredients.Add(new Ingredient() { Name = "Orange", Calorie = 60 });
+            ingredients.Add(new Ingredient() { Name = "Apple", Calorie = 95 });
+            int totalCalories = 0;
+            foreach(Ingredient i in ingredients)
+            {
+                totalCalories += i.Calorie;
+            }
+            model.TotalCalories = totalCalories;
+            // model.GoogleVisionResults = response.ToString();
+            //model.GoogleVisionResults = "Total calorie";
             // after successfully uploading redirect the user
-            return View("Index" ,model); 
+            model.Ingredients = ingredients;
+            return View("Index", model);
         }
     }
 }
--- website/AIAdventurers/AIAdventurers/Models/VisionViewModel.cs
+++ website/AIAdventurers/AIAdventurers/Models/VisionViewModel.cs
@@ -10,6 +10,16 @@ namespace AIAdventurers.Models
         public string imageUrl { get; set; }
 
         public string GoogleVisionResults { get; set; }
+
+        public List<Ingredient> Ingredients { get; set; }
+
+        public int TotalCalories { get; set; }
     }
 
+    public class Ingredient
+    {
+       public string Name { get; set; }
+            public int Calorie { get; set; }
+
+    }
 }
